e89a8d5858b6beef1ca4b4f7b850289d2a3ba16f
Sbessonov <52399828+Sbessonov@users.noreply.github.com>
2020-01-09 12:05:58 +0300

    fix

--- tree/test.py
+++ tree/test.py
@@ -1,20 +1,20 @@
 import unittest
-import test
+import tree
 import random
 
 
-def makect():
-    ct = test.RBTree()
-    ct.root = test.Node(3)
-    ct.root.left = test.Node(1, ct.root)
-    ct.root.right = test.Node(5, ct.root)
-    ct.root.left.left = test.Node(0, ct.root.left)
-    ct.root.left.right = test.Node(2, ct.root.left)
-    ct.root.right.left = test.Node(4, ct.root.right)
-    ct.root.right.right = test.Node(7, ct.root.right)
-    ct.root.right.right.left = test.Node(6, ct.root.right.right)
-    ct.root.right.right.right = test.Node(8, ct.root.right.right)
-    ct.root.right.right.right.right = test.Node(9, ct.root.right.right.right)
+def grow():
+    ct = tree.RBTree()
+    ct.root = tree.Node(3)
+    ct.root.left = tree.Node(1, ct.root)
+    ct.root.right = tree.Node(5, ct.root)
+    ct.root.left.left = tree.Node(0, ct.root.left)
+    ct.root.left.right = tree.Node(2, ct.root.left)
+    ct.root.right.left = tree.Node(4, ct.root.right)
+    ct.root.right.right = tree.Node(7, ct.root.right)
+    ct.root.right.right.left = tree.Node(6, ct.root.right.right)
+    ct.root.right.right.right = tree.Node(8, ct.root.right.right)
+    ct.root.right.right.right.right = tree.Node(9, ct.root.right.right.right)
     return ct
 
 
@@ -24,7 +24,7 @@ def only_keys_comparator(t1, t2):
     return False
 
 
-def tree_compare(t1: test.Node, t2: test.Node, comparator=only_keys_comparator):
+def tree_compare(t1: tree.Node, t2: tree.Node, comparator=only_keys_comparator):
     if t1 == None or t2 == None:
         return t1 == t2
     if not tree_compare(t1.left, t2.left):
@@ -36,25 +36,54 @@ def tree_compare(t1: test.Node, t2: test.Node, comparator=only_keys_comparator):
 
 class TestRBTree(unittest.TestCase):
     def test_creation(self):
-        t = test.RBTree()
+        t = tree.RBTree()
         self.assertNotEqual(t, None)
 
     def test_insert_one(self):
-        t = test.RBTree()
+        t = tree.RBTree()
         t.insert(2)
         self.assertEqual(t.root.key, 2)
 
     def test_find_none(self):
-        t = test.RBTree()
+        t = tree.RBTree()
         self.assertFalse(t.find(2))
 
     def test_find_one(self):
-        t = test.RBTree()
+        t = tree.RBTree()
         t.insert(2)
         self.assertTrue(t.find(2))
 
     def test_find_none_small(self):
-        t = test.RBTree()
+        t = tree.RBTree()
         t.insert(2)
         self.assertFalse(t.find(1))
 
+    def test_find_none_big(self):
+        t = tree.RBTree()
+        t.insert(2)
+        self.assertFalse(t.find(3))
+
+    def test_insert_many(self):
+        t = tree.RBTree()
+        for i in range(10):
+            t.insert(i)
+        ct = grow()
+        self.assertTrue(tree_compare(t.root, ct.root))
+
+    def test_find_many(self):
+        t = tree.RBTree()
+        for i in range(0, 100, 3):
+            t.insert(i)
+        for i in range(1, 100, 3):
+            t.insert(i)
+        for i in range(2, 100, 3):
+            t.insert(i)
+        for i in range(10):
+            self.assertTrue(t.find(i))
+
+    def test_find_many2(self):
+        t = tree.RBTree()
+        for i in range(10, 0, -1):
+            t.insert(i)
+        for i in range(1, 11):
+            self.assertTrue(t.find(i))
